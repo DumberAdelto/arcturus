f55fd0077b3f61c440ae2f57e0a6e6c968d550b6
DumberAdelto <sainiaarav805@gmail.com>
2024-10-05 19:53:40 +0530

    add countdown and tagline

--- script.js
+++ script.js
@@ -86,3 +86,36 @@ document.addEventListener("DOMContentLoaded", () => {
     initStars();
     requestAnimationFrame(updateAndDrawStars);
 });
+
+
+// COUNTDOWN
+
+var countDownDate = new Date("Nov 2, 2024").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+//   document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+//   + minutes + "m " + seconds + "s left. Make your choice.";
+
+    document.getElementById('countdown').innerHTML = `<span style='font-family: monospace'>${days}</span> days, <span style='font-family: monospace'>${hours}</span> hours, <span style='font-family: monospace'>${minutes}</span> minutes and <span style='font-family: monospace'>${seconds}</span> seconds left.<br> Make your choice.`
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "The time is neigh,<br> to choose your side.";
+  }
+}, 1000);
